17220f603ff12b30a9188acceac4112baa60147c
Hasan Sattar <hasanraza1798@gmail.com>
2022-03-10 19:13:15 +0000

    created server and connected to db

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import mongoose from 'mongoose'
+import { errorHandlers } from './middleware/errorHandlers'
+
+const app = express()
+
+const { DB_CONNECTION, PORT } = process.env
+if (!DB_CONNECTION || !PORT) throw new Error('DB CONNECTION & PORT REQUIRED')
+
+const whitelist = ['http://localhost:3000']
+
+const corsOptions = {
+  origin: function (origin: any, callback: any) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+    },
+    credentials: true
+}
+
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(cookieParser())
+
+
+app.use(errorHandlers)
+
+mongoose.connect(DB_CONNECTION)
+
+mongoose.connection.on('connected', () => {
+    console.log('DB Connected')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
+
+mongoose.connection.on('error', err => console.log(err))
